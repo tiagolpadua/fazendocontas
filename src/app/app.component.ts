31d922734211112bad3b262a8999f997222d1396
Tiago Pádua <tiagolpadua@gmail.com>
2017-08-22 23:43:40 -0300

    tela inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
-import 'rxjs/add/operator/toPromise';
-import 'rxjs/add/operator/map';
-import { Ano } from './models/ano';
 
 @Component({
   selector: 'app-root',
@@ -11,23 +6,4 @@ import { Ano } from './models/ano';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  anos: Ano[];
-
-  constructor(private http: Http) {
-    this.getAnos()
-      .then(anos => {
-        this.anos = anos;
-        console.log(this.anos.length);
-      })
-      .catch(err => {
-        window.alert(err);
-      });
-  }
-
-  getAnos(): Promise<Ano[]> {
-    return this.http.get('/assets/api/anos.json')
-      .map((res: Response) => res.json())
-      .toPromise();
-  }
 }
--- src/app/fc-home/fc-home.component.ts
+++ src/app/fc-home/fc-home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
+import { Ano } from '../models/ano';
 
 @Component({
   selector: 'app-fc-home',
@@ -6,8 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fc-home.component.css']
 })
 export class FcHomeComponent implements OnInit {
+  anos: Ano[];
 
-  constructor() { }
+  constructor(private http: Http) {
+    this.getAnos()
+      .then(anos => {
+        this.anos = anos;
+        console.log(this.anos.length);
+      })
+      .catch(err => {
+        window.alert(err);
+      });
+  }
+
+  getAnos(): Promise<Ano[]> {
+    return this.http.get('/assets/api/anos.json')
+      .map((res: Response) => res.json())
+      .toPromise();
+  }
 
   ngOnInit() {
   }
